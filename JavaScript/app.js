bbdad125969c7b6c1673c89d8fd0115eacb9c55b
SoumenBaral <shumen.sb@gmail.com>
2023-11-23 08:48:20 +0600

    paly with objects ;

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -2,3 +2,17 @@ let x = 343.23223232
 // We need to take 2 number after decimal point 
 
 console.log(x.toFixed(2));
+
+const person = {
+    name : "Soumen",
+    friends: ["shuvo","shakib","Alamin","Anoara"],
+    shuvo_Activity: {
+        job: "Vadaima",
+        salary : null,
+        Age : 25
+    },
+    Age: 26
+
+}
+console.log(Object.keys(person) );
+console.log(Object.values(person.shuvo_Activity) );
