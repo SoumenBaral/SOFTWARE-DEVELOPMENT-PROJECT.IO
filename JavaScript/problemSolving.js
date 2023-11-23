5ffe54d5a2edea2972c66cf5fcc5aed2abdd1293
SoumenBaral <shumen.sb@gmail.com>
2023-11-23 11:46:35 +0600

    problem one done

---
+++ JavaScript/problemSolving.js
@@ -0,0 +1,23 @@
+const person = {
+    name : "Soumen",
+    friends: ["shuvo","shakib","Alamin","Anoara"],
+    shuvo_Activity: {
+        job: "Vadaima",
+        salary : null,
+        Age : 25
+    },
+    Age: 26
+}
+
+// make function that will return a the value is od odd or even 
+
+function OddOrEven(x) {
+    if(x%2==0){
+        console.log("Its a Even number");
+    }
+    else{
+        console.log("Its defiantly Odd number ");
+    }
+}
+
+OddOrEven(30)
