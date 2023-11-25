c109cddc09717918777e2e49b9e543a3c006531d
SoumenBaral <shumen.sb@gmail.com>
2023-11-25 10:23:14 +0600

    deposit btn done ; ;

--- JavaScript/2.Dom.js/Bappear Bank/app.js
+++ JavaScript/2.Dom.js/Bappear Bank/app.js
@@ -8,10 +8,27 @@ function inputValue(){
 
 function depositBtn(){
     let inputVal = inputValue()
-    console.log(inputVal);
+    const DepositTxt = document.getElementById("Deposit")
+    let Deposit = parseInt(DepositTxt.innerText)
+    console.log(Deposit);
+    if(inputVal>0){
+        Deposit += inputVal
+        DepositTxt.innerText = Deposit
+    }
+    else{
+        alert("Give a Valid amount")
+    }
+
+    const BalanceTxt = document.getElementById("Balance");
+    let Balance = parseInt(BalanceTxt.innerText)
+    if(inputVal>0){
+        Balance += inputVal
+        BalanceTxt.innerText = Balance
+    }
 }
 
 function withdrawBTN(){
     let inputVal = inputValue()
     console.log(inputVal);
+
 }
