3d6fcd1a09a3c0f6e1e7993997fd78dc979adfd2
SoumenBaral <shumen.sb@gmail.com>
2023-11-28 13:05:45 +0600

    inside the app use template done

--- 3. DJango/2. Working with Django Template/project2/firstApp/views.py
+++ 3. DJango/2. Working with Django Template/project2/firstApp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('hello world you are my only world I do not have any ')
+    # return HttpResponse('hello world you are my only world I do not have any ')
+    return render(request, "firstApp/home.html")
