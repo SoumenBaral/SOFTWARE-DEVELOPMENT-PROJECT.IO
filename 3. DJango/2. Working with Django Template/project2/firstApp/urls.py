c5a7011b080efc530b8ad34ee0b580b4cbc994d4
SoumenBaral <shumen.sb@gmail.com>
2023-11-28 12:41:50 +0600

    rander

--- 3. DJango/2. Working with Django Template/project2/firstApp/urls.py
+++ 3. DJango/2. Working with Django Template/project2/firstApp/urls.py
@@ -1,5 +1,10 @@
 
 from django.urls import path
+# ----------------------------
+# 3 ways to import views from app
+# from firstApp.views import home
+# from firstApp import views
+# And finlay that is running under the line 
 from . import  views
 urlpatterns = [
   
