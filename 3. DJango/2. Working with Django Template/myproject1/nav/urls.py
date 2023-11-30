742ad58705bc35fe77f1c804c9e626fe8578979d
SoumenBaral <shumen.sb@gmail.com>
2023-12-01 01:45:30 +0600

    all done

--- 3. DJango/2. Working with Django Template/myproject1/nav/urls.py
+++ 3. DJango/2. Working with Django Template/myproject1/nav/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path ('', views.navHome),
     path('about/',views.about),
-    path('contact/', views.contact)
+    path('contact/', views.contact),
+    path('blog/',views.blog),
+
 ]
