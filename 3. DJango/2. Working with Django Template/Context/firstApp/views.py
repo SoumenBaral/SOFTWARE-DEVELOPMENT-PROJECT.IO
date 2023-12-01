40ad6c59a7cc0b09f156a2ca5b8c1e55ca0d432d
SoumenBaral <shumen.sb@gmail.com>
2023-12-01 10:06:46 +0600

    done for now

--- 3. DJango/2. Working with Django Template/Context/firstApp/views.py
+++ 3. DJango/2. Working with Django Template/Context/firstApp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import datetime
 
 
 def Home(request):
     d = {"name":"Soumen","age":5,"list":[1,2,3,"ami",'tumi','danish'],
+         'amidate': datetime.datetime.now(),
          "course": [
              {
                  "Id":1,
@@ -21,7 +22,8 @@ def Home(request):
                  "name":"React",
                  "fee":5000                 
              },
-         ]
+         ],
+         'lst' : ['python','is','best'], 
          
          }
     return render(request,'Home.html',d)
