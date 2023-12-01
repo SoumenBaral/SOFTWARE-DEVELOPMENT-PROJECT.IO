b59a9c5d337a5fa5d69944fe285ab1a5b6c98bec
SoumenBaral <shumen.sb@gmail.com>
2023-12-01 08:58:48 +0600

    add a templates and connect it

--- 3. DJango/2. Working with Django Template/Context/firstApp/views.py
+++ 3. DJango/2. Working with Django Template/Context/firstApp/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
+def Home(request):
+    return render(request,'Home.html')
+
 def About (request):
     return HttpResponse("Ami to Vala na vala loia thiko ")
