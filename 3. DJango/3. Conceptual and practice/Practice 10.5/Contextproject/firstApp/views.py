f5d74f535aafa1e94a405791ce9ac7aab9b0d00a
SoumenBaral <shumen.sb@gmail.com>
2023-12-01 14:16:37 +0600

    all filter is done '

--- 3. DJango/3. Conceptual and practice/Practice 10.5/Contextproject/firstApp/views.py
+++ 3. DJango/3. Conceptual and practice/Practice 10.5/Contextproject/firstApp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from datetime import date
+from datetime import date ,datetime
 
 current_date = date.today()
-
+current_time = datetime.now().time()
 def AppHome (request):
     dec = {"fun":['python', 'is', 'fun'] , "today":current_date,'Nodata':"","adding":10,"MyName": "soumen",
            'fundus':"Mama Mia Man Make My Money Maker",
@@ -10,6 +10,7 @@ def AppHome (request):
                         {'name': 'Josh', 'age': 19},
                         {'name': 'Dave', 'age': 22},
                         {'name': 'Joe', 'age': 31},
-                    ]
+                    ],
+            "current_time":current_time
            }
     return render(request,'AppHome.html',dec)
