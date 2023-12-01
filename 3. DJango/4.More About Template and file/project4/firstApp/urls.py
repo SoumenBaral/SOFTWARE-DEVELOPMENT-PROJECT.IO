b73ed0a34e668da7d61127d5e7770fed9e588fa9
SoumenBaral <shumen.sb@gmail.com>
2023-12-02 00:18:08 +0600

    get the value form url

--- 3. DJango/4.More About Template and file/project4/firstApp/urls.py
+++ 3. DJango/4.More About Template and file/project4/firstApp/urls.py
@@ -3,5 +3,6 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('',views.AppHome)
+    path('',views.AppHome ,name ="Home" ),
+    path("about/<int:id>/", views.about, name="about")
 ]
