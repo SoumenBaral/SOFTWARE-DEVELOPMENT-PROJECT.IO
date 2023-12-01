81dbdf7fc967afa3d113194d8154baa2fdf7d010
SoumenBaral <shumen.sb@gmail.com>
2023-12-01 15:00:08 +0600

    all done

--- 3. DJango/3. Conceptual and practice/Practice 10.5/Navigarion/NavApp/urls.py
+++ 3. DJango/3. Conceptual and practice/Practice 10.5/Navigarion/NavApp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.NavHome),
+    path('contact/',views.contact),
+    path('about/',views.about)
    
 ]
--- 3. DJango/3. Conceptual and practice/Practice 10.5/Navigarion/NavApp/views.py
+++ 3. DJango/3. Conceptual and practice/Practice 10.5/Navigarion/NavApp/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def NavHome(request):
     return render(request,"NavHome.html")
+
+def contact(request):
+    return render(request,"Contact.html")
+
+def about(request):
+    return render(request,"About.html")
