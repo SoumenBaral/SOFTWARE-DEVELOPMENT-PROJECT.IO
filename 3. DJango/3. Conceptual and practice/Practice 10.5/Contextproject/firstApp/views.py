d609a4d751b119b80f4dbbef8b5cda8c8f06e418
SoumenBaral <shumen.sb@gmail.com>
2023-12-01 13:27:22 +0600

    Cut

--- 3. DJango/3. Conceptual and practice/Practice 10.5/Contextproject/firstApp/views.py
+++ 3. DJango/3. Conceptual and practice/Practice 10.5/Contextproject/firstApp/views.py
@@ -4,5 +4,5 @@ from datetime import date
 current_date = date.today()
 
 def AppHome (request):
-    dec = {"fun":['python', 'is', 'fun'] , "today":current_date,'Nodata':"","adding":10}
+    dec = {"fun":['python', 'is', 'fun'] , "today":current_date,'Nodata':"","adding":10,"MyName": "soumen",'fundus':"Mama Mia Man Make My Money Maker"}
     return render(request,'AppHome.html',dec)
