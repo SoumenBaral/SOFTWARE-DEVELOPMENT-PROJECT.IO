76c2edbb37a531355930fd51e5ee8d8aea37490a
SoumenBaral <shumen.sb@gmail.com>
2023-12-01 09:09:15 +0600

    add data from context

--- 3. DJango/2. Working with Django Template/Context/firstApp/views.py
+++ 3. DJango/2. Working with Django Template/Context/firstApp/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 
 
 def Home(request):
-    return render(request,'Home.html')
+    d = {"name":"Soumen","Age":25,"list":[1,2,3,"ami",'tumi','danish']}
+    return render(request,'Home.html',d)
 
 def About (request):
     return HttpResponse("Ami to Vala na vala loia thiko ")
