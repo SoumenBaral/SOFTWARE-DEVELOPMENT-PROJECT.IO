c2f8e32f323cc5ab95613a3ec5f455646ea25e16
SoumenBaral <shumen.sb@gmail.com>
2023-12-01 13:46:03 +0600

    upper lower title and list ;

--- 3. DJango/3. Conceptual and practice/Practice 10.5/Contextproject/firstApp/views.py
+++ 3. DJango/3. Conceptual and practice/Practice 10.5/Contextproject/firstApp/views.py
@@ -4,5 +4,12 @@ from datetime import date
 current_date = date.today()
 
 def AppHome (request):
-    dec = {"fun":['python', 'is', 'fun'] , "today":current_date,'Nodata':"","adding":10,"MyName": "soumen",'fundus':"Mama Mia Man Make My Money Maker"}
+    dec = {"fun":['python', 'is', 'fun'] , "today":current_date,'Nodata':"","adding":10,"MyName": "soumen",
+           'fundus':"Mama Mia Man Make My Money Maker",
+           "sortBy" : [
+                        {'name': 'Josh', 'age': 19},
+                        {'name': 'Dave', 'age': 22},
+                        {'name': 'Joe', 'age': 31},
+                    ]
+           }
     return render(request,'AppHome.html',dec)
