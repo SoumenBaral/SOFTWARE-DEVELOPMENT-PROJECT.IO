8a1fdd15d84069741324ffa7fc831429e9744cc8
SoumenBaral <shumen.sb@gmail.com>
2023-12-01 09:39:37 +0600

    for loop done

--- 3. DJango/2. Working with Django Template/Context/firstApp/views.py
+++ 3. DJango/2. Working with Django Template/Context/firstApp/views.py
@@ -4,7 +4,26 @@ from django.http import HttpResponse
 
 
 def Home(request):
-    d = {"name":"Soumen","age":5,"list":[1,2,3,"ami",'tumi','danish']}
+    d = {"name":"Soumen","age":5,"list":[1,2,3,"ami",'tumi','danish'],
+         "course": [
+             {
+                 "Id":1,
+                 "name":"Python",
+                 "fee":5000                 
+             },
+             {
+                 "Id":2,
+                 "name":"django",
+                 "fee":15000                 
+             },
+             {
+                 "Id":3,
+                 "name":"React",
+                 "fee":5000                 
+             },
+         ]
+         
+         }
     return render(request,'Home.html',d)
 
 def About (request):
