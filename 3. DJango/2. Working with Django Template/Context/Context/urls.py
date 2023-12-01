a4821b15f08dcb091276a17b57895363dc4be24d
SoumenBaral <shumen.sb@gmail.com>
2023-12-01 08:47:14 +0600

    run dev

--- 3. DJango/2. Working with Django Template/Context/Context/urls.py
+++ 3. DJango/2. Working with Django Template/Context/Context/urls.py
@@ -3,5 +3,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("firstapp/",include("firstapp.urls")),
+    path("firstapp/",include("firstApp.urls")),
 ]
