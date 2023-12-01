ab9f9c5a00302a06df5037f949160f1ea42e928b
SoumenBaral <shumen.sb@gmail.com>
2023-12-01 13:07:39 +0600

    defalt done

--- 3. DJango/3. Conceptual and practice/Practice 10.5/Contextproject/firstApp/views.py
+++ 3. DJango/3. Conceptual and practice/Practice 10.5/Contextproject/firstApp/views.py
@@ -4,5 +4,5 @@ from datetime import date
 current_date = date.today()
 
 def AppHome (request):
-    dec = {"fun":['python', 'is', 'fun'] , "today":current_date}
+    dec = {"fun":['python', 'is', 'fun'] , "today":current_date,'Nodata':""}
     return render(request,'AppHome.html',dec)
