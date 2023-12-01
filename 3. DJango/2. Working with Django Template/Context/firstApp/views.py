e8eba4345301cc15bee480545403e635dbe78cfd
SoumenBaral <shumen.sb@gmail.com>
2023-12-01 08:44:11 +0600

    connect withe urls and views

--- 3. DJango/2. Working with Django Template/Context/firstApp/views.py
+++ 3. DJango/2. Working with Django Template/Context/firstApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def About (request):
+    return HttpResponse("Ami to Vala na vala loia thiko ")
