3b0315a804cdba037b35a42a2310bdd10a2a7788
SoumenBaral <shumen.sb@gmail.com>
2023-12-02 09:25:58 +0600

    work on meal and about ;

--- 3. DJango/Assignment/foodyBD/foodyBD/urls.py
+++ 3. DJango/Assignment/foodyBD/foodyBD/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.Home),
+    path('',views.Home,name="home"),
+    path('about/',include('about_us.urls'))
 ]
