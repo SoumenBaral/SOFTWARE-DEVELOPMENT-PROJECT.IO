7e96386f2800e0f24005623c1c6c367f703959fa
SoumenBaral <shumen.sb@gmail.com>
2023-12-03 18:50:14 +0600

    can watch the form value in the terminal ;

--- 4. Working with Models/1.Forms in Django/project5/firstApp/views.py
+++ 4. Working with Models/1.Forms in Django/project5/firstApp/views.py
@@ -5,4 +5,6 @@ def About(request):
     return render(request,'about.html')
 
 def Form (request):
+    if(request.method == "POST"):
+        print(request.method)
     return render(request,'Form.html')
