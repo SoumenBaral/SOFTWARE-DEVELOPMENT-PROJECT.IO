be0295c0b37d2339ee08f98d8db9e3f2cc7a8d0b
SoumenBaral <shumen.sb@gmail.com>
2023-12-03 20:35:15 +0600

    learn lost of forms formet

--- 4. Working with Models/1.Forms in Django/project5/firstApp/forms.py
+++ 4. Working with Models/1.Forms in Django/project5/firstApp/forms.py
@@ -3,3 +3,13 @@ from django import forms
 class contactForm(forms.Form):
     Name = forms.CharField(label='Name')
     Email = forms.EmailField(label='Email')
+    Age = forms.IntegerField()
+    Weight = forms.FloatField()
+    Balance = forms.DecimalField()
+    checked = forms.BooleanField()
+    Birthday = forms.DateField()
+    Appointment =forms.DateTimeField() 
+    CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    Size = forms.ChoiceField(choices=CHOICES)
+
+    pizza = forms.MultipleChoiceField(choices=CHOICES)
