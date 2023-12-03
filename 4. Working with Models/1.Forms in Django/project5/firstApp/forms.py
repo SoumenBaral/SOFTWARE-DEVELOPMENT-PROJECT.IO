ec306aeceb12865e6bdafb0f2921da7baa4e9d99
SoumenBaral <shumen.sb@gmail.com>
2023-12-03 21:21:50 +0600

    add Student data Class

--- 4. Working with Models/1.Forms in Django/project5/firstApp/forms.py
+++ 4. Working with Models/1.Forms in Django/project5/firstApp/forms.py
@@ -1,15 +1,33 @@
 from django import forms
 
 class contactForm(forms.Form):
-    Name = forms.CharField(label='Name')
-    Email = forms.EmailField(label='Email')
-    Age = forms.IntegerField()
-    Weight = forms.FloatField()
-    Balance = forms.DecimalField()
-    checked = forms.BooleanField()
-    Birthday = forms.DateField()
-    Appointment =forms.DateTimeField() 
-    CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
-    Size = forms.ChoiceField(choices=CHOICES)
+    Name = forms.CharField( label="Full Name : ", help_text="Total length must be within 70 characters", required=False, error_messages={'required': 'Please enter your name.'},widget = forms.Textarea(attrs = {'id' : 'text_area', 'class' : 'class1 class 2', 'placeholder' : 'Enter your name'},))
+    # Email = forms.EmailField(label='Email')
+    # Age = forms.IntegerField()
+    # Weight = forms.FloatField()
+    # Balance = forms.DecimalField()
+    # checked = forms.BooleanField()
+    # Birthday = forms.DateField()
+    # Appointment =forms.DateTimeField() 
+    # CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    # Size = forms.ChoiceField(choices=CHOICES)
 
-    pizza = forms.MultipleChoiceField(choices=CHOICES)
+    # pizza = forms.MultipleChoiceField(choices=CHOICES)
+
+    file = forms.FileField()
+
+
+
+
+class StudentData(forms.Form):
+    name =forms.CharField(widget=forms.TextInput)
+    email =forms.CharField(widget=forms.EmailInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        valname = self.cleaned_data['name']
+        valemail = self.cleaned_data['email']
+        if len(valname) < 10:
+            raise forms.ValidationError("Enter a name with at least 10 characters")    
+        if '.com' not in valemail:
+            raise forms.ValidationError("Your email must contain .com")
--- 4. Working with Models/1.Forms in Django/project5/firstApp/views.py
+++ 4. Working with Models/1.Forms in Django/project5/firstApp/views.py
@@ -14,8 +14,36 @@ def Form (request):
     return render(request,'Form.html')
 
 
+# def DjangoForm(request):
+#     form = contactForm(request.POST)
+#     if form.is_valid():
+#         print(form.cleaned_data)
+#     return render(request,'django_form.html',{"form":form})
+# want to upload file 
+
+# def DjangoForm(request):
+#     form = contactForm(request.POST,request.FILES)
+#     if form.is_valid():
+#         file =form.cleaned_data['file']
+#         with open('./firstApp/uploads/' + file.name, 'wb+') as destination:
+#             for chunk in file.chunks():
+#                 destination.write(chunk)
+#         print(form.cleaned_data)
+#     else:
+#         form = contactForm()
+
+#     return render(request,'django_form.html',{"form":form})
+
+
 def DjangoForm(request):
-    form = contactForm(request.POST)
-    if form.is_valid():
-        print(form.cleaned_data)
-    return render(request,'django_form.html',{"form":form})
+    if request.method == 'POST':
+        form = contactForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = form.cleaned_data['file']
+            with open('./firstApp/uploads' + file.name, 'wb+') as destination:
+                for chunk in file.chunks():
+                    destination.write(chunk)
+            print(form.cleaned_data)
+    else:
+        form = contactForm()
+    return render(request, 'django_form.html', {'form':form}) 
