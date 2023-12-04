8ef9a65181f3deeaa4b8157f6bd96554688855fc
SoumenBaral <shumen.sb@gmail.com>
2023-12-04 22:54:07 +0600

    make a modles that is mainly a table of a database

--- 4. Working with Models/2.Models and ModleForm in Django/project7/firstApp/models.py
+++ 4. Working with Models/2.Models and ModleForm in Django/project7/firstApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    roll = models.IntegerField(primary_key=True)
+    address = models.TextField()
+    
