77e5fd0b3e0991a5775e504070cc56cb0bb1d5d4
SoumenBaral <shumen.sb@gmail.com>
2023-12-05 03:34:53 +0600

    done for today'

--- 4. Working with Models/2.Models and ModleForm in Django/project7/firstApp/urls.py
+++ 4. Working with Models/2.Models and ModleForm in Django/project7/firstApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
    path('', views.form,name="homepage"),
-   path('delete/<int:roll>',views.delete_student ,name = "delete_student")
+   path('delete/<int:roll>',views.delete_student ,name = "delete_student"),
+    path('add/', views.add_student, name="add_student"),
 ]
--- 4. Working with Models/2.Models and ModleForm in Django/project7/firstApp/views.py
+++ 4. Working with Models/2.Models and ModleForm in Django/project7/firstApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from . import models
+from . import models,forms
 # Create your views here.
 def form(request):
     student = models.Student.objects.all()
@@ -9,3 +9,13 @@ def delete_student(request,roll):
     std = models.Student.objects.get(pk=roll).delete()
     print(std)
     return redirect("homepage")
+
+def add_student(request):
+    if request.method == 'POST':
+        form = forms.StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("homepage")
+    else :
+        form = forms.StudentForm()
+    return render(request, 'add_student.html', {'form' : form})
