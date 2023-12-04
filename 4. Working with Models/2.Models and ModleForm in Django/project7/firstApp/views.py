d46ca27191555fddafdf5e972dd987cacc0deeb3
SoumenBaral <shumen.sb@gmail.com>
2023-12-04 23:35:42 +0600

    add student db

--- 4. Working with Models/2.Models and ModleForm in Django/project7/firstApp/views.py
+++ 4. Working with Models/2.Models and ModleForm in Django/project7/firstApp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from . import models
 # Create your views here.
 def form(request):
-
+    student = models.Student.objects.all()
+    print(student)
     return render(request,'form.html')
