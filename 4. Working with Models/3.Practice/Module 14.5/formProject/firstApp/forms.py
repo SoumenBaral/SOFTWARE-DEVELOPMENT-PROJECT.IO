3170fc453291804781e962ad99554604680d1366
SoumenBaral <shumen.sb@gmail.com>
2023-12-05 10:17:44 +0600

    add some form field

--- 4. Working with Models/3.Practice/Module 14.5/formProject/firstApp/forms.py
+++ 4. Working with Models/3.Practice/Module 14.5/formProject/firstApp/forms.py
@@ -1,6 +1,15 @@
 from django import forms
-
+from django.forms.widgets import NumberInput
 
 class MyContact(forms.Form):
     Name = forms.CharField()
     Comment = forms.CharField(widget=forms.Textarea)
+    LittleComment = forms.CharField(widget=forms.Textarea(attrs={'rows':3}))
+    Email = forms.EmailField()
+    Agree = forms.BooleanField()
+    Date = forms.DateField()
+    BirthDay = forms.DateField(widget=NumberInput(attrs={'type': 'date'}))
+    BIRTH_YEAR_CHOICES = ['1980', '1981', '1982']
+    BirthYear = forms.DateField(widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES))
+    message = forms.CharField(max_length = 10,)
+    first_name = forms.CharField(initial='Your name')
