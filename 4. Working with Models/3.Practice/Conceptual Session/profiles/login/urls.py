ca044cf5f7f7619e551838619f6d4e9c8ec8e895
SoumenBaral <shumen.sb@gmail.com>
2023-12-06 13:35:09 +0600

    facting some problem

--- 4. Working with Models/3.Practice/Conceptual Session/profiles/login/urls.py
+++ 4. Working with Models/3.Practice/Conceptual Session/profiles/login/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.Home ,name="home"),
-    path('login',views.Login , name='login')
+    path('login/',views.Login , name='login')
 ]
--- 4. Working with Models/3.Practice/Conceptual Session/profiles/login/views.py
+++ 4. Working with Models/3.Practice/Conceptual Session/profiles/login/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def Home(request):
-    return HttpResponse("<h1 style='text-align:center;'>ami to vala na </h1>")
+    # return HttpResponse("<h1 style='text-align:center;'>ami to vala na </h1>")
+    return render(request,'home.html')
 
 def Login(request):
     return render(request,'login.html')
