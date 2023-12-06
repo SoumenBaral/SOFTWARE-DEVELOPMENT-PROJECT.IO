6b9d4b8cb52ae2b42fca49551ac7b5a5c91d5c60
SoumenBaral <shumen.sb@gmail.com>
2023-12-06 12:13:25 +0600

    add base html and home and login

--- 4. Working with Models/3.Practice/Conceptual Session/profiles/login/views.py
+++ 4. Working with Models/3.Practice/Conceptual Session/profiles/login/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def Home(request):
+    return HttpResponse("<h1 style='text-align:center;'>ami to vala na </h1>")
+
+def Login(request):
+    return render(request,'login.html')
