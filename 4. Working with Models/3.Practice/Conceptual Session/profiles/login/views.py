bb722c3939b7cce58c2a2e783221cb34e16c9178
SoumenBaral <shumen.sb@gmail.com>
2023-12-06 17:28:40 +0600

    from work done

--- 4. Working with Models/3.Practice/Conceptual Session/profiles/login/views.py
+++ 4. Working with Models/3.Practice/Conceptual Session/profiles/login/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def Home(request):
     # return HttpResponse("<h1 style='text-align:center;'>ami to vala na </h1>")
+   
     return render(request,'home.html')
 
 def Login(request):
+    if request.method == 'POST':
+        name = request.POST.get('username')
+        Email = request.POST.get('email')
+        select = request.POST.get('select')
+        return render(request,"home.html",{'name' : name, 'email': Email, 'select' : select})
+
+    
+
     return render(request,'login.html')
