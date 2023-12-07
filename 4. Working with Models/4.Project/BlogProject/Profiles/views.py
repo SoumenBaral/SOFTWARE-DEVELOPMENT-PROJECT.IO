ebd696e339da0e995e3523c5858f7d9d00057f79
SoumenBaral <shumen.sb@gmail.com>
2023-12-07 14:23:41 +0600

    successfully add date to database via website

--- 4. Working with Models/4.Project/BlogProject/Profiles/views.py
+++ 4. Working with Models/4.Project/BlogProject/Profiles/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from . import  forms
+
+def AddAuthor(request):
+    if request.method == 'POST':
+        form =forms.AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('addAuthor')
+        
+    else:
+        form = forms.AuthorForm
+        return render(request,'Author.html',{"form":form})
 
 def AddProfiles(request):
     return render(request,'profiles.html')
