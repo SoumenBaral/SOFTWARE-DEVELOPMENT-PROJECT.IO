d2f4442dde60b6963440808697d894fa4b02fd88
SoumenBaral <shumen.sb@gmail.com>
2023-12-07 14:20:08 +0600

    successfully add data form the website

--- 4. Working with Models/4.Project/BlogProject/Author/urls.py
+++ 4. Working with Models/4.Project/BlogProject/Author/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add/',views.AddAuthor,name="addAuthor")
+    path('add/',views.AddAuthor,name='addAuthor')
 ]
--- 4. Working with Models/4.Project/BlogProject/Author/views.py
+++ 4. Working with Models/4.Project/BlogProject/Author/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from .forms import AuthorForm
+from django.shortcuts import render,redirect
+from . import  forms
 
 def AddAuthor(request):
-    form = AuthorForm
-    return render(request,'Author.html',{"form":form})
+    if request.method == 'POST':
+        form =forms.AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('addAuthor')
+        
+    else:
+        form = forms.AuthorForm
+        return render(request,'Author.html',{"form":form})
