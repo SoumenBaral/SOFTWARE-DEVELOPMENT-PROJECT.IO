260a1606bcae380528be3f864b9ac37cd6c76068
SoumenBaral <shumen.sb@gmail.com>
2023-12-08 16:09:38 +0600

    make mode of musician and form and views work

--- 4. Working with Models/5.Practice/Module 15.5/MusiciansDirectory/musician/models.py
+++ 4. Working with Models/5.Practice/Module 15.5/MusiciansDirectory/musician/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Musician(models.Model):
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    Email = models.EmailField(max_length=200)
+    PhoneNumber = models.IntegerField(max_length=11)
+    INSTRUMENT_CHOICES = [
+        ('guitar', 'Guitar'),
+        ('piano', 'Piano'),
+        ('drums', 'Drums'),
+        
+    ]
+    
+    Instruments = models.ManyToManyField('Instrument',)
+    def __str__ (self) -> str:
+        return self.FirstName
+
+
+class Instrument(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- 4. Working with Models/5.Practice/Module 15.5/MusiciansDirectory/musician/views.py
+++ 4. Working with Models/5.Practice/Module 15.5/MusiciansDirectory/musician/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
+def AddMusician(request):
+    form = forms.AddMusician
+    return render(request,'musician.html',{"form":form})
