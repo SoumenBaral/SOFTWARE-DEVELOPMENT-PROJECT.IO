c7b12d527dadee0015792ce2159a0c18ee591268
SoumenBaral <shumen.sb@gmail.com>
2023-12-08 10:38:28 +0600

    Curd Create is done

--- 4. Working with Models/4.Project/BlogProject/Posts/views.py
+++ 4. Working with Models/4.Project/BlogProject/Posts/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from . import forms
 
 # Create your views here.
 def AddPost(request):
-    return  render(request,'Posts.html')
+    if request.method == 'POST':
+        form = forms.AddPost(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('addPost')
+    else:
+        form = forms.AddPost
+        return  render(request,'Posts.html',{"form":form})
