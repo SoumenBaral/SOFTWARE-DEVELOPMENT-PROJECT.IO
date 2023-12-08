50a3ac1a801a4badc089a920cb4a7201437b7355
SoumenBaral <shumen.sb@gmail.com>
2023-12-08 16:22:07 +0600

    register the side to database

--- 4. Working with Models/5.Practice/Module 15.5/MusiciansDirectory/musician/models.py
+++ 4. Working with Models/5.Practice/Module 15.5/MusiciansDirectory/musician/models.py
@@ -5,14 +5,7 @@ class Musician(models.Model):
     FirstName = models.CharField(max_length=100)
     LastName = models.CharField(max_length=100)
     Email = models.EmailField(max_length=200)
-    PhoneNumber = models.IntegerField(max_length=11)
-    INSTRUMENT_CHOICES = [
-        ('guitar', 'Guitar'),
-        ('piano', 'Piano'),
-        ('drums', 'Drums'),
-        
-    ]
-    
+    PhoneNumber = models.CharField(max_length=11)
     Instruments = models.ManyToManyField('Instrument',)
     def __str__ (self) -> str:
         return self.FirstName
