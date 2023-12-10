e9713ff8b0204efb01b0c1ed5071e8f5c86ceee8
SoumenBaral <shumen.sb@gmail.com>
2023-12-10 22:04:05 +0600

    work with singUp form

---
+++ 5. Aouth and Class Based view/1. Authentication VS Authorization/EightProject/FirstApp/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+
+class RegistrationForm(forms.ModelForm):
+    FirstName = forms.CharField(widget=forms.TextInput(attrs={'id' : 'required'}))
+    LastName = forms.CharField(widget=forms.TextInput(attrs={'id': 'required'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'id' : 'required'}))
+ 
+
+    class Meta:
+        model = User
+        fields = ("username","FirstName","LastName","email")
--- 5. Aouth and Class Based view/1. Authentication VS Authorization/EightProject/FirstApp/views.py
+++ 5. Aouth and Class Based view/1. Authentication VS Authorization/EightProject/FirstApp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegistrationForm
 
 # Create your views here.
+def Home(request):
+    return render(request,'home.html')
+
+def signUp(request):
+    pass
