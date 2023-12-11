34e7258d432fc3e9aaeed8cf8fb4f0f56e3cedc6
SoumenBaral <shumen.sb@gmail.com>
2023-12-12 02:09:51 +0600

    singup and login authentication

--- 5. Aouth and Class Based view/1. Authentication VS Authorization/EightProject/FirstApp/views.py
+++ 5. Aouth and Class Based view/1. Authentication VS Authorization/EightProject/FirstApp/views.py
@@ -9,36 +9,38 @@ def Home(request):
     return render(request,'home.html')
 
 def signUp(request):
-    # if not request.user.is_authenticated:
+    if not request.user.is_authenticated:
         if request.method == 'POST':
             form = RegistrationForm(request.POST)
             if form.is_valid():
                 messages.success(request,"Account Created Successfully")
                 form.save()
                 print(form.cleaned_data)
-                # return redirect('home')
+                return redirect('login')
             
         else :
             form = RegistrationForm()
         return render(request,'signUp.html',{"form":form})
-    # else:
-    #     return redirect('profile')
+    else:
+        return redirect('profile')
 
 def user_login(request):
-   if request.method == "POST":
-       form = AuthenticationForm(request=request,data=request.POST)
-       if form.is_valid():
-            name = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(username=name,password=password)
-            login(request,user)
-            messages.warning(request,"LoggedIn Successfully ")
-            return redirect("home")
-
-
-   else:
-       form = AuthenticationForm()
-       return render(request,"login.html",{'form':form})
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = AuthenticationForm(request=request,data=request.POST)
+            if form.is_valid():
+                    name = form.cleaned_data['username']
+                    password = form.cleaned_data['password']
+                    user = authenticate(username=name,password=password)
+                    login(request,user)
+                    messages.warning(request,"LoggedIn Successfully ")
+                    return redirect("home")
+  
+        else:
+            form = AuthenticationForm()
+            return render(request,"login.html",{'form':form})
+    else: 
+       return redirect("profile")
 
 
 def profile(request):
