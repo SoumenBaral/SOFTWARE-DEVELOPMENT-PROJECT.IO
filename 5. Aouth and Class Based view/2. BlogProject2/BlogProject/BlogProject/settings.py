655eb72b5bcc3de37227af1b36ec959f227cbd70
SoumenBaral <shumen.sb@gmail.com>
2023-12-13 14:00:02 +0600

    work with home page

--- 5. Aouth and Class Based view/2. BlogProject2/BlogProject/BlogProject/settings.py
+++ 5. Aouth and Class Based view/2. BlogProject2/BlogProject/BlogProject/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+LOGIN_URL = '/author/login/' 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -124,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- 5. Aouth and Class Based view/2. BlogProject2/BlogProject/BlogProject/views.py
+++ 5. Aouth and Class Based view/2. BlogProject2/BlogProject/BlogProject/views.py
@@ -3,10 +3,11 @@ from Posts.models import Posts,Category
 
 def Home(request):
     data = Posts.objects.all()
+    Categories = Category.objects.all()
     # print(data)
     # for i in data:
     #     print(i.Name)
     #     for j in i.Category.all():
     #         print(j)
 
-    return render(request,'home.html',{"data":data})
+    return render(request,'home.html',{"data":data,"Categories":Categories})
