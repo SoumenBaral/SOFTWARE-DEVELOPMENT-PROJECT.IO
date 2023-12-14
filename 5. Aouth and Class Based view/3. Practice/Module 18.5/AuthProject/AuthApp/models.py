aae429f54ed02dd649167aa15189a85af596c06c
SoumenBaral <shumen.sb@gmail.com>
2023-12-14 21:02:11 +0600

    finally start

--- 5. Aouth and Class Based view/3. Practice/Module 18.5/AuthProject/AuthApp/models.py
+++ 5. Aouth and Class Based view/3. Practice/Module 18.5/AuthProject/AuthApp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-# It's ok to have it i will handle it man don't worry worry worry worry don't worry it okk man it ok whats happened I don't know 
-# if you have any confusion about me please let me inform 
+
+
+
+
