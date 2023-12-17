e1c44b33bf9b6e553577bb375dc372b365abe8cd
SoumenBaral <shumen.sb@gmail.com>
2023-12-17 23:16:16 +0600

    Modify home class base view

--- 5. Aouth and Class Based view/6. Assignment/CarZone/CarZone/views.py
+++ 5. Aouth and Class Based view/6. Assignment/CarZone/CarZone/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-def home (request):
-    return render(request,'Home.html')
 
 class Home(TemplateView):
     template_name = 'Home.html'
