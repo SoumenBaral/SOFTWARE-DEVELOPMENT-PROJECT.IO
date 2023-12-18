7ec9edaac0cd79e9e77ff53846ab12ced4e7c2b6
SoumenBaral <shumen.sb@gmail.com>
2023-12-18 06:41:14 +0600

    add 2 app

--- 5. Aouth and Class Based view/6. Assignment/CarZone/CarZone/settings.py
+++ 5. Aouth and Class Based view/6. Assignment/CarZone/CarZone/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'UserAuth',
+    'carModel',
+    'carsBrand',
     "crispy_forms",
     "crispy_bootstrap5", 
 ]
