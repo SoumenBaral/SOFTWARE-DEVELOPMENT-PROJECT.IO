e370b1b9497fcdcfd83e6f1f1019d4ec9e61d3d1
SoumenBaral <shumen.sb@gmail.com>
2024-04-07 02:04:32 +0600

    added lots

--- 5. Aouth and Class Based view/4. BlogProject3/Cookie/firstApp/urls.py
+++ 5. Aouth and Class Based view/4. BlogProject3/Cookie/firstApp/urls.py
@@ -3,8 +3,12 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
+    # path('',views.setCookie),
     path('',views.setCookie),
     path('get/',views.getCookies),
     path('del/',views.deleteCookies),
+    path('sets/',views.setSession),
+    path('gets/',views.get_session),
+    path('dels/',views.del_session),
 
 ]
--- 5. Aouth and Class Based view/4. BlogProject3/Cookie/firstApp/views.py
+++ 5. Aouth and Class Based view/4. BlogProject3/Cookie/firstApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from datetime import datetime , timedelta
-
+from django.http import HttpResponse
 # Create your views here.
 def setCookie(request):
     response = render(request,'Home.html')
@@ -20,8 +20,22 @@ def deleteCookies(request):
     return response
 
 def setSession(request):
-    data = {
-        'name' : 'rahim',
-        'age' : 23,
-        'language' : 'Bangla'
-      }
+    request.session['name'] = 'Karim'
+    return render(request,'set_session.html')
+
+def get_session(request):
+    if 'name' in request.session:
+        name = request.session.get('name', 'Guest')
+        request.session.modified = True
+        return render(request,'get_session.html' ,{'name' : name})
+    else:
+        return HttpResponse("Your session has been expired.Login again")
+
+
+    # name = request.session.get('name', 'Guest')
+    # return render(request,'get_session.html' ,{'name' : name})
+        
+def del_session(request):
+    # del request.session['name']
+    request.session.flush()
+    return render(request,'del_session.html')
